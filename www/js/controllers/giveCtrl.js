6fc83af9641e4884d40ae27b4553e7c6c2e6b890
Bill Randall <william.randall@cru.org>
2015-05-08 16:42:07 -0400

    Add modal list selector for amount

--- www/js/controllers/giveCtrl.js
+++ www/js/controllers/giveCtrl.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('demo.give', [])
-    .controller('GiveCtrl', function($scope, $stateParams, designations) {
+    .controller('GiveCtrl', function($scope, $stateParams, $ionicModal, designations) {
         $scope.designationToGiveTo = designations.get($stateParams.designationNumber);
+
+        $ionicModal.fromTemplateUrl('amounts-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
     });
